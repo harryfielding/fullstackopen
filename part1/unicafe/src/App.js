40ff15c3e3c250e215af6337b90e70ec3bfe60be
harryfielding <harfie22@student.loreto.ac.uk>
2023-02-11 18:33:29 +0000

    Add good/neutral/bad buttons

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,11 +6,40 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  console.log("app rerendered")
+  console.log("good: ", good)
+  console.log("neutral: ", neutral)
+  console.log("bad: ", bad)
+
   return (
     <div>
-      code here
+      <Feedback good={good} setGood={setGood} 
+                neutral={neutral} setNeutral={setNeutral}
+                bad={bad} setBad={setBad} />
     </div>
   )
 }
 
+const Feedback = (props) => {
+  const clickGood = () => props.setGood(props.good+1)
+  const clickNeutral = () => props.setNeutral(props.neutral+1)
+  const clickBad = () => props.setBad(props.bad+1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button label="good" onClick={clickGood} />
+      <Button label="neutral" onClick={clickNeutral} />
+      <Button label="bad" onClick={clickBad} />
+    </div>
+  )
+}
+
+const Button = ({ label, onClick }) => {
+  return (
+    <>
+      <button onClick={onClick}>{label}</button>
+    </>
+  )
+}
 export default App
