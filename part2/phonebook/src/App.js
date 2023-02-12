c85921c4e1c288c561bdc2b419c2a56125999b7d
harryfielding <harfie22@student.loreto.ac.uk>
2023-02-12 23:57:44 +0000

    Exercises 2.6 and 2.7 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+const nameChanged = (event) => setNewName(event.target.value)
+
+const addName = (event) => {
+  event.preventDefault()
+  const personObject = {
+    name: newName
+  }
+  if (persons.some((a) => a.name === personObject.name)) {
+    alert(`${newName} is already in the phonebook`)
+  } else {
+    setPersons(persons.concat(personObject))
+  }
 }
 
-export default App;
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input onChange={nameChanged} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <table>
+        <thead><tr><th><h2>Numbers</h2></th></tr></thead>
+        <tbody>
+          {persons.map((person) => <tr key={person.name}><th>{person.name}</th></tr>)}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
