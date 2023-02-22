70e603147ff4a7bafc85b4a61bfb84eb128073c6
harryfielding <harfie22@student.loreto.ac.uk>
2023-02-22 23:16:02 +0000

    2.20 complete

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Country = ({ countries, filter, setFilter }) => {
+const Country = ({ countries, filter, setFilter, weatherData }) => {
+
   const showCountry = (country) => {
     return () => {
       setFilter(country)
     }
   }
 
-  if (countries.filter(country => country.name.common === filter).length === 1) {
+  if (countries.filter(country => country.name.common === filter).length === 1 || countries.length === 1) {
     return (
       <div>
         <h1>{countries[0].name.common}</h1>
@@ -22,6 +23,7 @@ const Country = ({ countries, filter, setFilter }) => {
         </ul>
         <h2>Flag:</h2>
         <img src={countries[0].flags.png} alt={countries[0].flags.alt} />
+        <Weather weatherData={weatherData} country={countries[0]} />
       </div>
     )
   } else if (countries.length > 10) {
@@ -31,9 +33,27 @@ const Country = ({ countries, filter, setFilter }) => {
   } 
 }
 
+const Weather = ({ weatherData, country }) => {
+  if (Object.keys(weatherData).length === 0) {
+    return null
+  }
+  return (
+    <div>
+      <h2>Weather in {country.capital[0]}:</h2>
+      <img src={`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`} />
+      <p>{weatherData.current.weather[0].description.charAt(0).toUpperCase() + weatherData.current.weather[0].description.slice(1)}</p>
+      <ul>
+        <li>Temperature: {Math.round((weatherData.current.temp-273.15) * 100) / 100}°C (feels like {Math.round((weatherData.current.feels_like-273.15) * 100) / 100}°C)</li>
+        <li>Wind speed: {weatherData.current.wind_speed}</li>
+      </ul>
+    </div>
+  )
+}
+
 const App = () => {
   const [filter, setFilter] = useState('')
   const [countries, setCountries] = useState([])
+  const [weatherData, setWeatherData] = useState({})
 
   useEffect(() => {
     axios
@@ -43,12 +63,22 @@ const App = () => {
     })
   }, [filter])
 
+  useEffect(() => {
+    if(countries.length === 1) {
+      axios
+      .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${countries[0].capitalInfo.latlng[0]}&lon=${countries[0].capitalInfo.latlng[1]}&appid=${process.env.REACT_APP_API_KEY}`)
+      .then(response => {
+        setWeatherData(response.data)
+      })
+    }
+  }, [countries])
+
   const filterChanged = event => setFilter(event.target.value)
 
   return (
     <div>
       search: <input onChange={filterChanged} />
-      <Country countries={countries} setFilter={setFilter} filter={filter}/>
+      <Country countries={countries} setFilter={setFilter} filter={filter} weatherData={weatherData} />
     </div>
   );
 }
