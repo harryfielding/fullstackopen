86fea1b760a717083484141bad13230324d2f9e6
harryfielding <harfie22@student.loreto.ac.uk>
2023-02-23 19:10:56 +0000

    3.3 complete

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -2,30 +2,36 @@ const express = require('express')
 const app = express()
 
 let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/api/persons', (req, res) => {
-    res.json(phonebook)
+  res.json(phonebook)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = phonebook.find(person => person.id === id)
+  res.json(person)
 })
 
 app.get('/info', (req, res) => {
