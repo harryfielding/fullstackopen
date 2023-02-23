605415e084eee83e7027d924576c867cf010613f
harryfielding <harfie22@student.loreto.ac.uk>
2023-02-23 20:21:22 +0000

    3.4 complete

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -31,7 +31,17 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = phonebook.find(person => person.id === id)
-  res.json(person)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  phonebook = phonebook.filter(person => person.id !== id)
+  res.status(204).end()
 })
 
 app.get('/info', (req, res) => {
