0a1bfff5ee2771bba5244eb5632f89463f445137
harryfielding <harfie22@student.loreto.ac.uk>
2023-02-26 18:46:19 +0000

    Add extra validation

--- part3/phonebook/backend/models/Person.js
+++ part3/phonebook/backend/models/Person.js
@@ -15,8 +15,21 @@ mongoose.connect(uri)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: v => {
+                return /\d{2,3}-\d+|\d+/.test(v)
+            }
+        },
+        required: true
+    },
 })
 
 personSchema.set('toJSON', {
