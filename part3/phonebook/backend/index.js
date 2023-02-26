4d674cd8b6dff7346222982e72cdd84e98c7f3d2
harryfielding <harfie22@student.loreto.ac.uk>
2023-02-26 18:31:26 +0000

    3.18 complete

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -5,29 +5,6 @@ const cors = require('cors')
 const Person = require('./models/Person')
 const app = express()
 
-let phonebook = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
@@ -51,7 +28,11 @@ app.get('/api/persons', (req, res, next) => {
 app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
-      res.json(person)
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
@@ -77,12 +58,6 @@ app.post('/api/persons', (req, res, next) => {
     })
   }
 
-  if (phonebook.find(person => person.name === req.body.name)) {
-    return res.status(400).json({
-      error: "Name already taken"
-    })
-  }
-
   const person = new Person({
     name: req.body.name,
     number: req.body.number
@@ -103,10 +78,14 @@ app.put('/api/persons/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-  let date = new Date()  
-  res.send(`<p>Phonebook has info for ${phonebook.length} people.</p>
-            <p>Time: ${date.toUTCString()}</p>`)
+app.get('/info', (req, res, next) => {
+  Person.find({})
+    .then(persons => {
+      let date = new Date()
+      res.send(`<p>Phonebook has info for ${persons.length} people.</p>
+      <p>Time: ${date.toUTCString()}</p>`)
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
