77f77bf3f0f5a9adce462f6b234b5be1af7a088b
harryfielding <harfie22@student.loreto.ac.uk>
2023-02-26 17:38:22 +0000

    Rename folder to models

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const Person = require('./modules/Person')
+const Person = require('./models/Person')
 const app = express()
 
 let phonebook = [
